6d81e6c73cd24668ba4f5aa9504559ee3a003a9f
thetoan99 <93066650+geoburn@users.noreply.github.com>
2021-11-02 14:50:07 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+     
+       <h1>Learn React Hooks</h1>
+
+    </div>
+  );
+}
+
+export default App;
